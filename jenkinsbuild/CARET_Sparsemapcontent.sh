3362894018e0cd2fbfea5419d24fa1beb1df8387
msn <daniel@caret.cam.ac.uk>
2011-12-06 13:36:39 +0000

    Minor tweaks

--- jenkinsbuild/CARET_Sparsemapcontent.sh
+++ jenkinsbuild/CARET_Sparsemapcontent.sh
@@ -10,18 +10,18 @@ if [ "${BUILD_STEP}" = "pre" ] ; then
     echo "INFO: Running pre flight check for version: ${VERSION}";
     CHECK_VERSION=`grep ${VERSION} pom.xml`;
     if [ "${?}" != "0" ] ; then
-        echo "ERROR: Version \"${VERSION}\" not found in pom.xml, aborting!"
+        echo "ERROR: Version \"${VERSION}\" not found in pom.xml, aborting!";
         exit 1;
     fi
 else
     if [ "${BUILD_STEP}" = "post" ] ; then
         echo "INFO: Running post build setup for version: ${VERSION}";
-        NAME="org/sakaiproject/nakamura/org.sakaiproject.nakamura.core"
-        URL="maven2.caret.cam.ac.uk"
+        NAME="org/sakaiproject/nakamura/org.sakaiproject.nakamura.core";
+        URL="maven2.caret.cam.ac.uk";
         echo "INFO: Making directory /var/www/${URL}/htdocs/${NAME}/${VERSION} on ${URL}";
         ssh ${URL} "mkdir -p /var/www/${URL}/htdocs/${NAME}/${VERSION}";
         echo "INFO: Synching ~/.m2/repository/${NAME}/${VERSION} to ${URL}:/var/www/${URL}/htdocs/${NAME}/";
-        rsync -av --delete ~/.m2/repository/${NAME}/${VERSION} ${URL}:/var/www/${URL}/htdocs/${NAME}/
+        rsync -av --delete ~/.m2/repository/${NAME}/${VERSION} ${URL}:/var/www/${URL}/htdocs/${NAME}/;
     else
         echo "ERROR: Invalid build step: \"${BUILD_STEP}\" specified";
         echo "ERROR: Please use ${0} (pre|post)";
